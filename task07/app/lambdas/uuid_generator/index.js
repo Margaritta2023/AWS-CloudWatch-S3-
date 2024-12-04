5521a25ed76d897db3d1d0bce8d0eab152355561
Margarita Mkrtchyan <margarita_mkrtchyan@epam.com>
2024-12-04 20:37:26 +0400

    initial commit

---
+++ task07/app/lambdas/uuid_generator/index.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const s3 = new AWS.S3();
+
+const bucketName = process.env.S3_BUCKET_NAME
+
+exports.handler = async (event) => {
+
+    try {
+        const uuidArray = Array.from({length : 10}, () => uuid.v4())
+    
+        const timestamp = new Date().toISOString();
+        const fileName = `${timestamp}`;
+
+        const s3Obj = { ids: uuidArray}
+   
+   
+        const s3Parameters = {
+           bucket: bucketName,
+           key:fileName,
+           body: JSON.stringify(s3Obj),
+           ContentType:'application/json'
+        };
+
+        const s3BResponse = await s3.putObject(s3Parameters).promise();
+
+        return {
+            statusCode:20,
+            body:JSON.stringify({
+                message: 'uuids successfully generated and uploaded to S3',
+                fileName:fileName,
+                s3BResponse:s3BResponse
+
+            })
+        };
+        } catch (error) {
+         console.log("Eror generating and uploading to S3:", error)
+         return {
+            statusCode:500,
+            body:JSON.stringify({
+                message: 'Failed to  generate and upload to S3:',
+                error:error.message
+            })
+         }
+        }
+       
+
+    }
+
+
+
+
+
